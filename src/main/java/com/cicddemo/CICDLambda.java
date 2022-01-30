ecaef076cb6d558210dc59636a3a0ef46c2ace94
vishakha22061998 <98169297+vishakha22061998@users.noreply.github.com>
2022-01-30 15:50:43 +0530

    Initial commit

---
+++ src/main/java/com/cicddemo/CICDLambda.java
@@ -0,0 +1,14 @@
+package com.cicddemo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class CICDLambda implements RequestHandler<Object, String>
+{
+
+	public String handleRequest(Object input, Context context) {
+		context.getLogger().log("Input:" + input);
+		return "Hello from CICD lambda!.......";
+	}
+	
+}
